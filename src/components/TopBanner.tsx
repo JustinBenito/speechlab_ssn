b25e448c2135f88715e485705631ed3a450d8b5b
JustinBenito <justinbenito1974@gmail.com>
2026-08-13 00:59:06 +0530

    fix: underline my company

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -9,7 +9,7 @@ export function TopBanner() {
       className="fixed inset-x-0 top-0 z-[60] flex h-8 items-center justify-center overflow-hidden"
     >
       <Image src="/banner.png" alt="" fill priority className="object-cover" />
-      <span className="relative font-display text-xs font-medium tracking-wide text-white">
+      <span className="relative font-display text-xs underline font-medium tracking-wide text-white">
         Website built by Alumnus of Speech Lab SSN
       </span>
     </a>
